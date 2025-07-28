3fa8fb5364c5a30dc260d01e1a9727fe05e98e8f
Nate Buckareff <nate.buckareff@gmail.com>
2025-07-27 20:37:33 -0700

    Add posthog

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,18 @@
 import { MetaProvider } from '@solidjs/meta';
 import { Router } from '@solidjs/router';
-import { Suspense } from 'solid-js';
+import { onMount, Suspense } from 'solid-js';
 import { FileRoutes } from '@solidjs/start/router';
+import posthog from 'posthog-js';
 import './app.css';
 
 export default function App() {
+  onMount(() => {
+    posthog.init('phc_e8Gkm9i9HlQouMduDjKXUsHVEl033DfplPsp3ccZXl4', {
+      api_host: 'https://us.i.posthog.com',
+      person_profiles: 'always',
+    });
+  });
+
   return (
     <Router
       base={import.meta.env.SERVER_BASE_URL}
